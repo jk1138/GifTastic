78067f528c60d4a8405d34891bb0aaea84699523
jyk <47995007+jk1138@users.noreply.github.com>
2019-07-06 07:39:15 -0400

    stil doesn't work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,57 @@
+// array of topics -----------> emotions <-----------
+var emotionsArray = ["happy", "sad", "angry", "stressed", "fear", "excited", "crying","confused"];
 
-  <script type="text/javascript">
-    $(".gif").on("click", function() {
-      // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-      var state = $(this).attr("data-state");
-      // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-      // Then, set the image's data-state to animate
-      // Else set src to the data-still value
-      if (state === "still") {
-        $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", "animate");
-      } else {
-        $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("data-state", "still");
-      }
-    });
+// Don't render page until done
+$(document).ready(function() {
+  for (var i = 0; i < emotionsArray.length; i++) {
+    $("#emotion-buttons").append("<button type='button' onclick='searchGif(\"" + emotionsArray[i] + "\")' class='btn btn-primary' value=' " + emotionsArray[i] + "'> " + emotionsArray[i] + " </button>");
+}
+});
 
+function clickMe() {
+var userInput = $('#additional-input').val();
+searchGif(userInput);
+}
+
+function clickSearch() {
+var userInput = $('#additional-input').val();
+
+if (userInput) {
+    $('#emotion-buttons').append("<button type='button' onclick='searchGif(\"" + userInput + "\")' class='btn btn-primary' value=' " + userInput + "'> " + userInput + " </button>");
+}
+}
+
+function searchGif(gifName) {
+$.ajax({
+        url: 'https://api.giphy.com/v1/gifs/search?q= ' + gifName + ' &api_key=JAenPKR6CVqXxCY2GvjrhzIYzY4Eq2m8',
+        type: 'GET',
+    })
+    .done(function(response) {
+        displayGif(response);
+    })
+}
+
+function displayGif(response) {
+$('#showResults').empty();
+for (var i = 0; i < response.data.length; i++) {
+    var rating = "<div class='ratings'> Rating:  " + (response.data[i].rating) + " </div>";
+    var image = rating + '<img src= " ' + response.data[i].images.fixed_height_still.url +
+        '" data-still=" ' + response.data[i].images.fixed_height_still.url +
+        ' " data-animate=" ' + response.data[i].images.fixed_height.url + '" data-state="still" class="movingImage" style= "width:250px; height:250px">';
+
+    image = '<div class="col-md-4">' + image + "</div>";
+    $('#showResults').append(image);
+}
+
+$('.movingImage').on('click', function() {
+    var state = $(this).attr('data-state');
+    if (state == 'still') {
+        $(this).attr('src', $(this).attr("data-animate"));
+        $(this).attr('data-state', 'animate');
+    } else {
+        $(this).attr('src', $(this).attr("data-still"));
+        $(this).attr('data-state', 'still');
+    }
+
+});
+}
